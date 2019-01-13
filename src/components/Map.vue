6f0c6499aa309d3716834f3d0111af5fc4af25ae
Lucas Hossack <lhossack@Lucass-MacBook-Pro.local>
2019-01-13 14:42:40 -0700

    add map page scaffolding

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,14 +1,28 @@
 <template>
-    <h2>{{msg}}</h2>
+    <div>
+        <map-view></map-view>
+        <map-layers></map-layers>
+        <map-maint-summary></map-maint-summary>
+        <map-selected-items></map-selected-items>
+        <map-crew-info></map-crew-info>
+    </div>
 </template>
 
 <script>
+import MapCrewInfo from './MapComponents/MapCrewInfo.vue'
+import MapLayers from './MapComponents/MapLayers.vue'
+import MapMaintenanceSummary from './MapComponents/MapMaintenanceSummary.vue'
+import MapSelectedItems from './MapComponents/MapSelectedItems.vue'
+import MapView from './MapComponents/MapView.vue'
+
 export default {
   name: 'Map',
-  data () {
-    return {
-      msg: 'Map goes here.. leaflet, anyone?'
-    }
+  components: {
+    'map-crew-info': MapCrewInfo,
+    'map-layers': MapLayers,
+    'map-maint-summary': MapMaintenanceSummary,
+    'map-selected-items': MapSelectedItems,
+    'map-view': MapView
   }
 }
 </script>
