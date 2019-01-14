1cd6b84b873c4ecec775db95d6d69cc46323ce55
Lucas Hossack <lhossack@ucalgary.ca>
2019-01-13 23:16:55 -0700

    Revert "add leaflet to build.. fails (webpack garbles vue single file components)"
    This reverts commit 7054ad53f23c52e670dd6f14ecb90c1319de7733.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
-        <map-view inline-template>
-          <div id="map"></div>
-        </map-view>
+        <map-view></map-view>
         <map-layers></map-layers>
         <map-maint-summary></map-maint-summary>
         <map-selected-items></map-selected-items>
@@ -15,7 +13,7 @@ import MapCrewInfo from './MapComponents/MapCrewInfo.vue'
 import MapLayers from './MapComponents/MapLayers.vue'
 import MapMaintenanceSummary from './MapComponents/MapMaintenanceSummary.vue'
 import MapSelectedItems from './MapComponents/MapSelectedItems.vue'
-import MapView from './MapComponents/MapView.js'
+import MapView from './MapComponents/MapView.vue'
 
 export default {
   name: 'Map',
--- src/components/MapComponents/MapView.vue
+++ src/components/MapComponents/MapView.vue
@@ -1,47 +1,15 @@
-<template lang="html">
-  <div>
-    <div id="map">
-    </div>
-  </div>
+<template>
+    <h2>Map View</h2>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      map: [],
-      markers: null
-    }
-  },
-  mounted () {
-    this.$nextTick(function () {
-      const map = L.map('map').setView([51.505, -0.09], 13)
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map)
-      L.marker([51.5, -0.09]).addTo(map).bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup()
-    })
-  }
+
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import "@/../../node_modules/leaflet/dist/leaflet.css"
-
-#map
-  width: 100%
-  height: 400px
-  font-weight: bold
-  font-size: 13px
-  text-shadow: 0 0 2px #fff
-  .leaflet-shadow-pane > .leaflet-marker-shadow
-    display: none
-</style>
-
-<!-- Add "scoped" attribute to limit CSS to this component only
-<style scoped>
-#mapid { height: 200px; }
-
 h1, h2 {
   font-weight: normal;
 }
@@ -57,4 +25,3 @@ a {
   color: #42b983;
 }
 </style>
--->
