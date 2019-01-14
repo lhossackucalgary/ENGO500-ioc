d4f06314af682fba056247beec910b4e144c2f78
Lucas Hossack <lhossack@ucalgary.ca>
2019-01-13 23:53:57 -0700

    Add open layers map to map view

--- src/components/MapComponents/MapView.vue
+++ src/components/MapComponents/MapView.vue
@@ -1,27 +1,42 @@
 <template>
-    <h2>Map View</h2>
+    <div id="map"></div>
 </template>
 
 <script>
-export default {
+import {Map, View} from 'ol'
+import TileLayer from 'ol/layer/Tile'
+import XYZ from 'ol/source/XYZ'
 
+export default {
+  mounted () {
+    var M = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    })
+    console.log(M)
+  }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+@import url("https://openlayers.org/en/latest/css/ol.css")
+
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
 }
 </style>
