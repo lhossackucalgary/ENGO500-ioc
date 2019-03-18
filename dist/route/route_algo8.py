926313281165b1cab07fd8bb1628078857278dc0
Deanna Ip <dwyip@ucalgary.ca>
2019-03-18 11:39:43 -0600

    algo considers urgent -> warning

--- dist/route/route_algo8.py
+++ dist/route/route_algo8.py
@@ -4,6 +4,7 @@ import pickle
 import requests
 import geopy.distance
 import math
+from collections import defaultdict
 
 headers = {"Authorization": "Basic bWFpbjoxYTZhZjZkOC1hMDc0LTVlNDgtOTNiYi04ZGY3MDllZDE3ODI="}
 
@@ -13,102 +14,88 @@ def main():
     """
     with open('./monitor_out.data', 'rb') as fin:
         data_in = pickle.load(fin)
-    crew_list = data_in["crew_list"]
+    crew_data = data_in["crew_list"]
+
+    crew_list = []
+    for crew in crew_data:
+        crew_list.append({'iotid': crew})
 
 
     """
-    Broken Bots Status data from monitor_out2.data
+    Broken bots status data from monitor_out2.data
     """
     with open('./monitor_out2.data', 'rb') as fin:
-        bot_stat = pickle.load(fin)
+        broken_bots = pickle.load(fin)
 
-    bot_urgent = []
-    bot_warning = []
+    broken_bots = [bot for bot in broken_bots if bot['status'] != 'Healthy']
 
-    for bot in bot_stat:
-        if bot['status'] == 'Urgent':
-            bot_urgent.append(bot['iotid'])
-        elif bot['status'] == 'Warning':
-            bot_warning.append(bot['iotid'])
-        else:
-            continue
-
-    print(bot_urgent)
-    print(bot_warning)
     """
     Obtain coordinates of crew and broken robots
     """
-    crew_coord = []
-    broken_bot_coord = []
     crew_error = []
-    bot_error = []
 
     for index in crew_list:
         try:
-            crew_loc = requests.get(url = "http://routescout.sensorup.com/v1.0/Things("+ str(index) + ")/Locations", headers = headers)
+            crew_loc = requests.get(url = "http://routescout.sensorup.com/v1.0/Things("+ str(index['iotid']) + ")/Locations", headers = headers)
             if (crew_loc.status_code >= 200) and (crew_loc.status_code < 300):
                 responseJSON = crew_loc.json()
                 if responseJSON['@iot.count'] == 0:
-                    crew_error.append(index)
+                    crew_error.append(index['iotid'])
                 else:
-                    crew_coord.append(responseJSON['value'][0]['location']['coordinates'])
+                    index['coord'] = responseJSON['value'][0]['location']['coordinates']
             else:
                 print("there was an error")
         except:
             logging.exception("Failed getting list of crew coordinates")
 
-    for item in crew_error:
-        while item in crew_list:
-            crew_list.remove(item)
+    crew_list = [crew for crew in crew_list if crew['iotid'] not in crew_error]
 
 
     for index in broken_bots:
         try:
-            broken_bot_loc = requests.get(url = "http://routescout.sensorup.com/v1.0/Things("+ str(index) + ")/Locations", headers = headers)
+            broken_bot_loc = requests.get(url = "http://routescout.sensorup.com/v1.0/Things("+ str(index['iotid']) + ")/Locations", headers = headers)
             if (broken_bot_loc.status_code >= 200) and (broken_bot_loc.status_code < 300):
                 responseJSON = broken_bot_loc.json()
-                if responseJSON['@iot.count'] == 0:
-                    bot_error.append(index)
-                else:
-                    broken_bot_coord.append(responseJSON['value'][0]['location']['coordinates'])
+                index['coord'] = responseJSON['value'][0]['location']['coordinates']
             else:
                 print("there was an error")
         except:
             logging.exception("Failed getting list of broken bots coordinates")
 
-    for item in bot_error:
-        while item in broken_bots:
-            broken_bots.remove(item)
-
 
     """
     Swap coordinate format to [lat,long]
     """
-    for i in range(0,len(crew_list)):
-        lon = crew_coord[i][0]
-        lat = crew_coord[i][1]
-        crew_coord[i][0] = lat
-        crew_coord[i][1] = lon
+    for crew in crew_list:
+        lon, lat = crew['coord'][0], crew['coord'][1]
+        crew['coord'] = lat, lon
 
-    for i in range(0,len(broken_bots)):
-        lon = broken_bot_coord[i][0]
-        lat = broken_bot_coord[i][1]
-        broken_bot_coord[i][0] = lat
-        broken_bot_coord[i][1] = lon
+    for bot in broken_bots:
+        lon, lat = bot['coord'][0], bot['coord'][1]
+        bot['coord'] = lat, lon
 
 
     """
     Calculate distance between crew and robot
     """
     routes = []
-    crew_routes = [[] for _ in range(0,len(crew_list))]
+    bot_urgent = []
+    bot_warning = []
 
-    while len(broken_bots)!=0:
+    for bot in broken_bots:
+        if bot['status'] == 'Urgent':
+            bot_urgent.append(bot)
+        elif bot['status'] == 'Warning':
+            bot_warning.append(bot)
+        else:
+            continue
+
+    while bot_urgent != []:
         cb_dist = []
-        for i in range(0,len(crew_list)):
-            for j in range(0,len(broken_bots)):
-                dist = geopy.distance.distance(broken_bot_coord[j],crew_coord[i]).km
-                cb_dist.append({'crewid': crew_list[i],'robotid': broken_bots[j], 'dist': dist})
+        for crew in crew_list:
+            for bot in bot_urgent:
+                dist = geopy.distance.distance(bot['coord'],crew['coord']).m
+                cb_dist.append({'crewid': crew['iotid'], 'robotid': bot['iotid'], 'dist': dist, 'botcoord': bot['coord']})
 
         min_dist = math.inf
         for route in cb_dist:
@@ -116,22 +103,50 @@ def main():
                 min_dist = route['dist']
                 min_route = route
 
-        crew_coord[crew_list.index(min_route['crewid'])] = broken_bot_coord[broken_bots.index(min_route['robotid'])]
-        del broken_bot_coord[broken_bots.index(min_route['robotid'])]
-        broken_bots.remove(min_route['robotid'])
+        for crew in crew_list:
+            if crew['iotid'] == min_route['crewid']:
+                crew['coord'] = min_route['botcoord']
+
+        bot_urgent = [bot for bot in bot_urgent if bot['iotid'] != min_route['robotid']]
+        routes.append(min_route)
+
+
+    while bot_warning != []:
+        cb_dist = []
+        for crew in crew_list:
+            for bot in bot_warning:
+                dist = geopy.distance.distance(bot['coord'],crew['coord']).m
+                cb_dist.append({'crewid': crew['iotid'], 'robotid': bot['iotid'], 'dist': dist, 'botcoord': bot['coord']})
+
+        min_dist = math.inf
+        for route in cb_dist:
+            if route['dist'] < min_dist:
+                min_dist = route['dist']
+                min_route = route
+
+        for crew in crew_list:
+            if crew['iotid'] == min_route['crewid']:
+                crew['coord'] = min_route['botcoord']
+
+        bot_warning = [bot for bot in bot_warning if bot['iotid'] != min_route['robotid']]
         routes.append(min_route)
 
 
     """
     Append crew routes
     """
+    crew_id = []
+    for crew in crew_list:
+        crew_id.append(crew['iotid'])
+
+    crew_routes = [[] for _ in range(0,len(crew_list))]
     for r in routes:
-        crew_routes[crew_list.index(r['crewid'])].append(r['robotid'])
+        crew_routes[crew_id.index(r['crewid'])].append(r['robotid'])
 
 
     """
     Uploading routes to server
-
+    """
 
     for index in range(0, len(crew_routes)):
         try:
@@ -143,7 +158,7 @@ def main():
                 logging.warning("HTTP Error code while adding route for crew %d" % crew_list[index])
         except:
                 logging.exception("Exception thrown generating route")
-    """
+
 
 """
 Logging data
