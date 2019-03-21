faaa51953cdbdc270990c63ccd063563ee65a8fb
Lucas Hossack <lhossack@ucalgary.ca>
2019-03-21 00:58:32 -0600

    crewtab updates everything properly! (Restructured mapview to be entirely defined in vue.. )

--- src/components/MapComponents/MapView.vue
+++ src/components/MapComponents/MapView.vue
@@ -19,220 +19,92 @@ import XYZ from 'ol/source/XYZ';
 import {fromLonLat, toLonLat} from 'ol/proj';
 import {Fill, Stroke, Style, Icon} from 'ol/style.js';
 
-function getLocations(listOfThings, outputMap) {
-  function getLocation(i) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = (function(id) {
-      return function() {
-        if (this.readyState == 4 && this.status == 200) {
-          var responseJSON = JSON.parse(xhttp.responseText);
-          if (responseJSON['@iot.count'] > 0) {
-            outputMap.set(id, responseJSON.value[0].location.coordinates);
-          } else {
-            // Remove thing id from list of things if it doesn't have a location..
-            var index = listOfThings.indexOf(id);
-            if (index > -1) {
-              listOfThings.splice(index, 1);
-            }
-          }
-        }
-      }
-    })(listOfThings[i]);
-
-    xhttp.open("GET", "http://routescout.sensorup.com/v1.0/Things(" + listOfThings[i] + ")/Locations", true);
-    xhttp.send();
-  }
-
-  for (var i = 0; i < listOfThings.length; i++) {
-    getLocation(i);
-  }
-}
-
-function getThings(things_props, thing_id_list, things_locations_map) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var things_list = JSON.parse(xhttp.responseText).value;
-      for (var t = 0; t < things_list.length; t++) {
-        thing_id_list.push(things_list[t]['@iot.id']);
-        things_props.set(things_list[t]['@iot.id'], things_list[t]);
-      }
-      getLocations(thing_id_list, things_locations_map);
-    }
-  };
-  xhttp.open("GET", "http://routescout.sensorup.com/v1.0/Things", true);
-  xhttp.send();
-}
-
-function LoadMapData(things, thing_ids, thing_locations, b_healthy_src, b_warning_src,
-                     b_urgent_src, b_unknown_src, b_needsparts_src, c_src, route_src) {
-  things.clear();
-  thing_ids.length = 0;
-  thing_locations.clear();
-  getThings(things, thing_ids, thing_locations);
-
-  function data_load_check() {
-
-
-    if (thing_ids.length > 0 && thing_locations.size == thing_ids.length) {
-      //console.log(things);
-      //console.log(thing_locations);
-
-      b_healthy_src.clear();
-      b_warning_src.clear();
-      b_urgent_src.clear();
-      b_unknown_src.clear();
-      b_needsparts_src.clear();
-      c_src.clear();
-      route_src.clear();
-
-      for (let i = 0; i < thing_ids.length; i++) {
-        // Create feature
-        let feature_s = new Feature(new Point(fromLonLat(thing_locations.get(thing_ids[i]))));
-        var thing_ = things.get(thing_ids[i]);
-        feature_s.setProperties(thing_);
-        var props_ = thing_["properties"];
-
-        // Assign to a layer source
-        if (typeof(props_["status"]) !== "undefined") {
-          // is a robot
-          if (props_["status"] === "Healthy") {
-            b_healthy_src.addFeature(feature_s);
-          } else if (props_["status"] === "Warning") {
-            b_warning_src.addFeature(feature_s);
-          } else if (props_["status"] === "Urgent") {
-            b_urgent_src.addFeature(feature_s);
-          } else if (props_["status"] === "Unknown") {
-            b_unknown_src.addFeature(feature_s);
-          } else if (props_["status"] === "Needs parts") {
-            b_needsparts_src.addFeature(feature_s);
-          }
-        }
-        if (typeof(props_["route"]) !== "undefined") { //(thing_["properties"]["route"] !== "undefined") {
-          // is a crew
-          c_src.addFeature(feature_s);
-          // also print their route to a layer..
-
-          if (props_["route"].length >= 1) {
-            // Add crew's location
-            var list_of_coords = [fromLonLat(thing_locations.get(thing_ids[i]))];
-
-            // Add location of each bot:
-            for (let i = 0; i < props_["route"].length; i++) {
-              list_of_coords.push(fromLonLat(thing_locations.get(props_["route"][i])));
-            }
-            var route_feature = new Feature(new LineString(list_of_coords));
-            route_feature.setProperties(thing_)
-            route_src.addFeature(route_feature);
-          }
-        }
-
-        /*        if is bot (true) {
-        } else if is crew (true) {
-        }*/
-      }
-      return;
-    } else {
-      setTimeout(data_load_check, 500);
-    }
-  }
-
-  data_load_check();
-}
-
-// var vectorSource = new VectorSource();
-var bots_healthy_source = new VectorSource();
-var bots_warning_source = new VectorSource();
-var bots_urgent_source = new VectorSource();
-var bots_unknown_source = new VectorSource();
-var bots_needsparts_source = new VectorSource();
-var crews_source = new VectorSource();
-var routes_source = new VectorSource();
-
-var bots_healthy_layer = new VectorLayer({
-  source: bots_healthy_source,
-  style: new Style({
-    image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-      anchor: [0.5, 0.5],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'fraction',
-      src: require('../../assets/Healthy.png'),
-      scale: 0.7
-    }))
-  })
-});
-var bots_warning_layer = new VectorLayer({
-  source: bots_warning_source,
-  style: new Style({
-    image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-      anchor: [0.5, 0.5],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'fraction',
-      src: require('../../assets/Warning.png'),
-      scale: 0.7
-    }))
-  })
-});
-var bots_urgent_layer = new VectorLayer({
-  source: bots_urgent_source,
-  style: new Style({
-    image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-      anchor: [0.5, 0.5],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'fraction',
-      src: require('../../assets/Urgent.png'),
-      scale: 0.7
-    }))
-  })
-});
-var bots_unknown_layer = new VectorLayer({
-  source: bots_unknown_source,
-  style: new Style({
-    image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-      anchor: [0.5, 0.5],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'fraction',
-      src: require('../../assets/Unknown.png'),
-      scale: 0.7
-    }))
-  })
-});
-var bots_needsparts_layer = new VectorLayer({
-  source: bots_needsparts_source,
-  style: new Style({
-    image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-      anchor: [0.5, 0.5],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'fraction',
-      src: require('../../assets/NeedsParts.png'),
-      scale: 0.7
-    }))
-  })
-});
-var crews_layer = new VectorLayer({
-  source: crews_source,
-  style: new Style({
-    image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-      anchor: [0.5, 0.5],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'fraction',
-      src: require('../../assets/Crew.png'),
-      scale: 0.7
-    }))
-  })
-});
-var routes_layer = new VectorLayer({
-  source: routes_source,
-  style: new Style({
-    stroke: new Stroke({
-      color: 'green',
-      width: 3
-    })
-  })
-});
 
 export default {
   mounted () {
+    var bots_healthy_layer = new VectorLayer({
+      source: this.bots_healthy_source,
+      style: new Style({
+        image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+          anchor: [0.5, 0.5],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'fraction',
+          src: require('../../assets/Healthy.png'),
+          scale: 0.7
+        }))
+      })
+    });
+    var bots_warning_layer = new VectorLayer({
+      source: this.bots_warning_source,
+      style: new Style({
+        image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+          anchor: [0.5, 0.5],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'fraction',
+          src: require('../../assets/Warning.png'),
+          scale: 0.7
+        }))
+      })
+    });
+    var bots_urgent_layer = new VectorLayer({
+      source: this.bots_urgent_source,
+      style: new Style({
+        image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+          anchor: [0.5, 0.5],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'fraction',
+          src: require('../../assets/Urgent.png'),
+          scale: 0.7
+        }))
+      })
+    });
+    var bots_unknown_layer = new VectorLayer({
+      source: this.bots_unknown_source,
+      style: new Style({
+        image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+          anchor: [0.5, 0.5],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'fraction',
+          src: require('../../assets/Unknown.png'),
+          scale: 0.7
+        }))
+      })
+    });
+    var bots_needsparts_layer = new VectorLayer({
+      source: this.bots_needsparts_source,
+      style: new Style({
+        image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+          anchor: [0.5, 0.5],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'fraction',
+          src: require('../../assets/NeedsParts.png'),
+          scale: 0.7
+        }))
+      })
+    });
+    var crews_layer = new VectorLayer({
+      source: this.crews_source,
+      style: new Style({
+        image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+          anchor: [0.5, 0.5],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'fraction',
+          src: require('../../assets/Crew.png'),
+          scale: 0.7
+        }))
+      })
+    });
+    var routes_layer = new VectorLayer({
+      source: this.routes_source,
+      style: new Style({
+        stroke: new Stroke({
+          color: 'green',
+          width: 3
+        })
+      })
+    });
+
+
     var M = new OLMap({
       target: 'map',
       layers: [
@@ -311,18 +183,8 @@ export default {
       }
     });
 
-    var thing_locations = new Map();  //map crew id to location
-    var things = new Map(); //map crew id to props
-    var thing_ids = [];
-    var send_single_data_req;
-    function sendDataRequest() {
-      // console.log("sent data requests");
-      LoadMapData(things, thing_ids, thing_locations, bots_healthy_source,
-                    bots_warning_source, bots_urgent_source, bots_unknown_source,
-                    bots_needsparts_source, crews_source, routes_source);
-      setTimeout(sendDataRequest, 60000);
-    };
-    sendDataRequest();
+
+    this.sendDataRequest();
   },
   props: {
     healthyLayerOn: Boolean,
@@ -336,7 +198,16 @@ export default {
   },
   data() {
     return {
-      send_single_data_req: Function
+      things: new Map(),
+      thing_ids: [],
+      thing_locations: new Map(),
+      bots_healthy_source: new VectorSource(),
+      bots_warning_source: new VectorSource(),
+      bots_urgent_source: new VectorSource(),
+      bots_unknown_source: new VectorSource(),
+      bots_needsparts_source: new VectorSource(),
+      crews_source: new VectorSource(),
+      routes_source: new VectorSource(),
     }
   },
   watch: {
@@ -362,34 +233,140 @@ export default {
       routes_layer.setVisible(newVal);
     },
     refreshRoutesWatcher: function(newVal, oldVal) {
+      this.LoadMapData(this.things, this.thing_ids, this.thing_locations, this.bots_healthy_source,
+                    this.bots_warning_source, this.bots_urgent_source, this.bots_unknown_source,
+                    this.bots_needsparts_source, this.crews_source, this.routes_source);
+    }
+  },
+  methods: {
+    sendDataRequest: function() {
+      // console.log("sent data requests");
+      this.LoadMapData(this.things, this.thing_ids, this.thing_locations, this.bots_healthy_source,
+                    this.bots_warning_source, this.bots_urgent_source, this.bots_unknown_source,
+                    this.bots_needsparts_source, this.crews_source, this.routes_source);
+      setTimeout(this.sendDataRequest, 60000);
+    },
 
-      let entries_crews = Object.entries(crews_source.undefIdIndex_);
-      for (let i = 0; i < entries_crews.length; i++) {
-        if (entries_crews[i][1].values_["@iot.id"] == newVal.iotid) {
-          entries_crews[i][1].values_.properties.route = newVal.route;
-        }
-      }
-
-      //routes_source
-      console.log(newVal);
-      let entries_routes = Object.entries(routes_source.undefIdIndex_);
-      for (let i = 0; i < entries_routes.length; i++) {
-        if (entries_routes[i][1].values_["@iot.id"] == newVal.iotid) {
-          if (newVal["route"].length >= 1) {
-            console.log(entries_routes[i][0]);
-            // // Add crew's location
-            // var list_of_coords = [fromLonLat(thing_locations.get(thing_ids[i]))];
-            //
-            // // Add location of each bot:
-            // for (let i = 0; i < props_["route"].length; i++) {
-            //   list_of_coords.push(fromLonLat(thing_locations.get(props_["route"][i])));
-            // }
-            // var route_feature = new Feature(new LineString(list_of_coords));
-            // route_feature.setProperties(thing_)
-            // routes_source.addFeature(route_feature);
+    getLocations: function(listOfThings, outputMap) {
+      function getLocation(i) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = (function(id) {
+          return function() {
+            if (this.readyState == 4 && this.status == 200) {
+              var responseJSON = JSON.parse(xhttp.responseText);
+              if (responseJSON['@iot.count'] > 0) {
+                outputMap.set(id, responseJSON.value[0].location.coordinates);
+              } else {
+                // Remove thing id from list of things if it doesn't have a location..
+                var index = listOfThings.indexOf(id);
+                if (index > -1) {
+                  listOfThings.splice(index, 1);
+                }
+              }
+            }
           }
+        })(listOfThings[i]);
+
+        xhttp.open("GET", "http://routescout.sensorup.com/v1.0/Things(" + listOfThings[i] + ")/Locations", true);
+        xhttp.send();
+      }
+
+      for (var i = 0; i < listOfThings.length; i++) {
+        getLocation(i);
+      }
+    },
+
+    getThings: function(things_props, thing_id_list, things_locations_map) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = (function (lthis) {
+      return function () {
+        if (this.readyState == 4 && this.status == 200) {
+          var things_list = JSON.parse(xhttp.responseText).value;
+          for (var t = 0; t < things_list.length; t++) {
+            thing_id_list.push(things_list[t]['@iot.id']);
+            things_props.set(things_list[t]['@iot.id'], things_list[t]);
+          }
+          lthis.getLocations(thing_id_list, things_locations_map);
+        }
+      }})(this);
+      xhttp.open("GET", "http://routescout.sensorup.com/v1.0/Things", true);
+      xhttp.send();
+    },
+
+    LoadMapData: function(things, thing_ids, thing_locations, b_healthy_src, b_warning_src,
+                         b_urgent_src, b_unknown_src, b_needsparts_src, c_src, route_src) {
+      things.clear();
+      thing_ids.length = 0;
+      thing_locations.clear();
+      this.getThings(things, thing_ids, thing_locations);
+
+      function data_load_check() {
+
+
+        if (thing_ids.length > 0 && thing_locations.size == thing_ids.length) {
+          //console.log(things);
+          //console.log(thing_locations);
+
+          b_healthy_src.clear();
+          b_warning_src.clear();
+          b_urgent_src.clear();
+          b_unknown_src.clear();
+          b_needsparts_src.clear();
+          c_src.clear();
+          route_src.clear();
+
+          for (let i = 0; i < thing_ids.length; i++) {
+            // Create feature
+            let feature_s = new Feature(new Point(fromLonLat(thing_locations.get(thing_ids[i]))));
+            var thing_ = things.get(thing_ids[i]);
+            feature_s.setProperties(thing_);
+            var props_ = thing_["properties"];
+
+            // Assign to a layer source
+            if (typeof(props_["status"]) !== "undefined") {
+              // is a robot
+              if (props_["status"] === "Healthy") {
+                b_healthy_src.addFeature(feature_s);
+              } else if (props_["status"] === "Warning") {
+                b_warning_src.addFeature(feature_s);
+              } else if (props_["status"] === "Urgent") {
+                b_urgent_src.addFeature(feature_s);
+              } else if (props_["status"] === "Unknown") {
+                b_unknown_src.addFeature(feature_s);
+              } else if (props_["status"] === "Needs parts") {
+                b_needsparts_src.addFeature(feature_s);
+              }
+            }
+            if (typeof(props_["route"]) !== "undefined") { //(thing_["properties"]["route"] !== "undefined") {
+              // is a crew
+              c_src.addFeature(feature_s);
+              // also print their route to a layer..
+
+              if (props_["route"].length >= 1) {
+                // Add crew's location
+                var list_of_coords = [fromLonLat(thing_locations.get(thing_ids[i]))];
+
+                // Add location of each bot:
+                for (let i = 0; i < props_["route"].length; i++) {
+                  list_of_coords.push(fromLonLat(thing_locations.get(props_["route"][i])));
+                }
+                var route_feature = new Feature(new LineString(list_of_coords));
+                route_feature.setProperties(thing_)
+                route_src.addFeature(route_feature);
+              }
+            }
+
+            /*        if is bot (true) {
+            } else if is crew (true) {
+            }*/
+          }
+          return;
+        } else {
+          setTimeout(data_load_check, 500);
         }
       }
+
+      data_load_check();
     }
   }
 }
