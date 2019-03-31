9a26600cbe7ff22c01a7f54d325f1f4bd5ad5553
Lucas Hossack <lhossack@ucalgary.ca>
2019-03-31 13:49:20 -0600

    Adjust robot layer order & route style

--- src/components/MapComponents/MapView.vue
+++ src/components/MapComponents/MapView.vue
@@ -105,7 +105,7 @@ export default {
       style: new Style({
         stroke: new Stroke({
           color: 'black',
-          lineDash: [0.1, 5],
+          lineDash: [0.7, 5],
           width: 3
         })
       })
@@ -119,13 +119,13 @@ export default {
             url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           })
         }),
-        this.crews_layer,
-        this.routes_layer,
         this.bots_healthy_layer,
+        this.routes_layer,
         this.bots_warning_layer,
         this.bots_urgent_layer,
         this.bots_unknown_layer,
-        this.bots_needsparts_layer
+        this.bots_needsparts_layer,
+        this.crews_layer
       ],
       view: new View({
         center: fromLonLat([-114,51]),
